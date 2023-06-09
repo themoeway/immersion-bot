eed060bc9292b4d85214e25298318eef0b3be12c
Timm <96317759+Timm04@users.noreply.github.com>
2023-06-09 19:54:44 +0200

    Disabled undo.py

--- cogs/undo.py
+++ cogs/undo.py
@@ -23,11 +23,12 @@ class Log(commands.Cog):
     @app_commands.checks.has_role("Moderator")
     @app_commands.choices(media = [Choice(name="Book", value="BOOK"), Choice(name="Manga", value="MANGA"), Choice(name="Visual Novel", value="VN"), Choice(name="Anime", value="ANIME"), Choice(name="Listening", value="LISTENING"), Choice(name="Readtime", value="READTIME")])
     async def undo(self, interaction: discord.Interaction, media: str, amount: int, message_link: str):
-        await interaction.response.defer(ephemeral=True)
-        message_link = message_link.split('/')
-        channel = await self.bot.fetch_channel(message_link[5])
-        message = await channel.fetch_message(message_link[6])
-        delete_query = f'DELETE FROM logs WHERE discord_user_id={interaction.user.id} AND media_type={media} AND amount={amount} AND created_at={message.created_at + timedelta(hours=1).strftime("%y-%m-%d %H:%M:%S.%f")}'
+        return
+#         await interaction.response.defer(ephemeral=True)
+#         message_link = message_link.split('/')
+#         channel = await self.bot.fetch_channel(message_link[5])
+#         message = await channel.fetch_message(message_link[6])
+#         delete_query = f'DELETE FROM logs WHERE discord_user_id={interaction.user.id} AND media_type={media} AND amount={amount} AND created_at={message.created_at + timedelta(hours=1).strftime("%y-%m-%d %H:%M:%S.%f")}'
         
         
 async def setup(bot: commands.Bot) -> None:
